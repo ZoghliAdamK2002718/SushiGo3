519c17c3b9c4250a02dcf95ab1c76e07823babe2
ZoghliAdamK2002718 <k2002718@students.katyisd.org>
2025-09-03 11:23:42 -0500

    Update SushiFrame.java

--- SushiFrame.java
+++ SushiFrame.java
@@ -1,9 +1,19 @@
 import javax.swing.JFrame;
 
 public class SushiFrame extends JFrame {
+	private static final int WIDTH = 1200;
+	private static final int HEIGHT = 480;
+	public SushiFrame(String title)
+	{
+		super(title);
+		setSize(WIDTH,HEIGHT);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SushiGoPanel panel = new SushiGoPanel("Sushi Go!");
+		setVisible(true);
+	}
 	public static void main(String[]args)
 	{
-		System.out.println("I love Mohammed Alsouri!-Adam");
+		
 	}
 	
 }
